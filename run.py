953d54eba825dea066ed5d40acc04d0cab86fd27
Oren Brigg <obrigg@cisco.com>
2021-12-19 10:02:43 +0200

    Optimized

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-import time
-import os
 import meraki
+from datetime import datetime, timedelta
 from rich import print as pp
 from rich.console import Console
 from rich.table import Table
@@ -58,75 +57,15 @@ def SelectNetwork():
     return(networks[int(selected)]['id'])
 
 
-    if client['status'] and client['ssid'] != None:
-        events = GetClientEvents(client)
-        channel = GetChannel(client, events)
-        if channel == '666':
-            clients_error.append(client)
-            if isDebug:
-                print("\t[yellow]Error: Not sure which channel client %s (ID: %s) is connected to[/yellow]" % (str(client['description']), client['id']))
-        elif int(channel) < 15:
-            client_capabilities = dashboard.client.getNetworkClient(NETWORK_ID, client['id'])
-            client_capabilities = meraki.clients.get_network_client({'network_id': NETWORK_ID, 'client_id': client['id']})['wirelessCapabilities']
-            if '5' in client_capabilities:
-                clients_5_on_2.append(client)
-                if isDebug:
-                    print('\tClient {:<40} ( Id: {:<8}) is on channel {:<3} [red]EVENTHOUGH IT IS 5GHz Capable!!![/red]'.format(str(client['description']), client['id'], channel))
-            elif isDebug:
-                print('\t[yellow]Client {:<40} ( Id: {:<8}) is on channel {:<3}, does not support 5GHz[/yellow]'.format(str(client['description']), client['id'], channel))
-            clients_2.append(client)
-        else:
-            clients_5.append(client)
-            if isDebug:
-                print('\tClient {:<40} ( Id: {:<8}) is on channel {:<3}'.format(str(client['description']), client['id'], channel))
-    elif isDebug:
-        print(f'\tClient %s is not a wireless client. Skipping...\n' % str(client['description']))
-
-    allEvents = []
-    per_page = 100
-    clientEvents_options = {
-        'network_id': NETWORK_ID,
-        'client_id': client['id'],
-        'per_page': per_page,
-        'starting_after': time.time() - 60*60*5 # Fetching logs 5 hours back.
-        }
-    isLastPage = False
-    while isLastPage == False:
-        try:
-            clientEvents = meraki.clients.get_network_client_events(clientEvents_options)
-            allEvents += clientEvents
-        except ValueError as e:
-            print(f'Error: {e}')
-        if len(clientEvents) < per_page:
-            isLastPage = True
-        else:
-            if isDebug:
-                print(f'\tClient: %s has %s events so far. Fetching more...' % (str(client['description']), len(allEvents)))
-            clientEvents_options['starting_after'] = clientEvents[per_page - 1]['occurredAt']
-    if isDebug:
-        print(f'\tClient: %s has a total of %s events' % (str(client['description']), len(allEvents)))
-    return(allEvents)
-
-    # The dashboard API does not have a direct query to the channel a client is
-    # associated with. The workaround is to analyze the client events and search
-    # for their last association event, where the channel association is mentioned.
-    # The achilles heel of the process is that clients associated for more than
-    # the searched time span will not return an associated channel.
-    # Increasing the time span will increase the running time of the code, while
-    # decreasing it will increase the number of unknown clients.
-    channel = '666'
-    for event in events:
-        if 'channel' in event['details'].keys():
-            channel = event['details']['channel']
-    return(channel)
-
 if __name__ == '__main__':
     # Initializing Meraki SDK
     dashboard = meraki.DashboardAPI(output_log=False, suppress_logging=True)
     NETWORK_ID = SelectNetwork()
 
     print("Fetching association events...")
-    association_events = dashboard.networks.getNetworkEvents(NETWORK_ID, includedEventTypes=["association"], perPage=1000, total_pages=50)
+    start_time = datetime.now() - timedelta(days=2) # Fetching association events for the last 2 days
+    association_events = dashboard.networks.getNetworkEvents(NETWORK_ID, productType="wireless", 
+        includedEventTypes=["association"], perPage=1000, total_pages=50, startingAfter=start_time.isoformat())
     results = {}
     """
     results format:
@@ -143,15 +82,17 @@ if __name__ == '__main__':
                 results[event['clientId']]['2.4GHz'] = True
             else:
                 results[event['clientId']] = {'2.4GHz': True, '5GHz': False, 'SSID': event['ssidName']}
+                results[event['clientId']]['name'] = event['clientDescription']
         # Client in 5GHz
         elif int(event['eventData']['channel']) > 13:
             if event['clientId'] in results.keys():
                 results[event['clientId']]['5GHz'] = True
             else:
                 results[event['clientId']] = {'2.4GHz': False, '5GHz': True, 'SSID': event['ssidName']}
+                results[event['clientId']]['name'] = event['clientDescription']
         # Client unknown
         else:
-            print(f"Error processing client {event['clientId']}")
+            print(f"Error processing client {event['clientId']}, unknown channel")
 
     # Adding client capabilities
     client_list = list(results.keys())
@@ -160,26 +101,21 @@ if __name__ == '__main__':
         try:
             client_details = dashboard.networks.getNetworkClient(NETWORK_ID, clientId=client)
             client_capabilities = client_details['wirelessCapabilities']
-            results[client]['name'] = client_details['description']
         except:
             client_capabilities = "Unknown"
             results[client]['name'] = "Unknown"
-            print(f"Error processing client {client}")
+            print(f"Error processing client {client}, client details not found")
         if '5' in client_capabilities:
             results[client]['5GHz_capable'] = True
-        elif '2.4' in client_capabilities:
-            results[client]['2.4GHz_capable'] = True
-            results[client]['5GHz_capable'] = False
         else:
             results[client]['5GHz_capable'] = False
-            results[client]['2.4GHz_capable'] = False
 
     # Summarize the numbers
     wirelessClientCount = len(results)
-    clients_2_only = [client for client in results if results[client]['2.4GHz'] == True and results[client]['5GHz'] == False]
+    clients_2_only = [client for client in results if results[client]['5GHz'] == False and results[client]['2.4GHz'] == True and results[client]['5GHz_capable'] == False]
     clients_5_only = [client for client in results if results[client]['5GHz'] == True and results[client]['2.4GHz'] == False]
     clients_error = [client for client in results if results[client]['5GHz'] == False and results[client]['2.4GHz'] == False]
-    clients_5_on_2 = [client for client in results if results[client]['2.4GHz'] == True and results[client]['5GHz_capable'] == True]
+    clients_5_on_2 = [client for client in results if results[client]['2.4GHz'] == True and (results[client]['5GHz_capable'] == True or results[client]['5GHz'] == True)]
     #
     if wirelessClientCount > 0:
         clients_2_percent = round(len(clients_2_only)*100/wirelessClientCount)
